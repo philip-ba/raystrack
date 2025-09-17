7f0bbb1c1c2639576033c13b2aab66808ee30c7e
Philip <philip.balizki@web.de>
2025-09-17 11:43:41 +0200

    feat(init update)

--- raystrack/__init__.py
+++ raystrack/__init__.py
@@ -1,9 +1,11 @@
 from .main import view_factor_matrix,  view_factor
-from .io import save_vf_matrix_json, load_vf_matrix_json
+from .io import save_vf_matrix_json, load_vf_matrix_json, save_meshes_json, load_meshes_json
 
 __all__ = [
     "view_factor_matrix",
     "view_factor",
     "save_vf_matrix_json",
     "load_vf_matrix_json",
+    "save_meshes_json",
+    "load_meshes_json"
 ]
